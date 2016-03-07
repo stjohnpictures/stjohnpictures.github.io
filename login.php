8045959cf1365a6574469234eb214d12945723ec
stjohnpictures <anthony.mattice@yahoo.com>
2016-03-07 17:59:24 -0600

    Update and rename login.php to login.html

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-<form action="proces.php" method="POST">
-  username: <input type="text" name="username" /></br>
-  password: <input type="password" name="pass" />
-  <input type="submit" value="Login!" />
-  
-</form>
-?>
