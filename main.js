d948bcf8d8f5641eb27a93a141341d9805e23f9f
stjohnpictures <anthony.mattice@yahoo.com>
2016-03-07 18:09:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-
+function clicked(){
+  
+  var user = document.getElementById('username');
+  var pass = document.getElementById('password');
+  
+  var coruser = "test";
+  var corpass = "123";
+  if(user.value == coruser){
+    if(pass.value == corpass){
+      window.alert("You are logged in as " + user.value);
+      
+    } else{
+      window.alert("Incorrect Username or Password!");
+    }
+  } else{
+    window.alert("Incorrect Username or Password!");
+  }
+  
+  
+  
+}
